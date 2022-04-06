dedc57a1f3252c5e3fb97f671df83b99c4f268ba
Tyler Reed <tylerreedytlearning@gmail.com>
2022-04-05 21:46:17 -0400

    Initial Commit

---
+++ RenovationProjectsTracker/RenovationProjectsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovationProjectsTrackerApp.swift
+//  RenovationProjectsTracker
+//
+//  Created by Tyler Reed on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct RenovationProjectsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
